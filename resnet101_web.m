4e3afe8bbf5441e14cc65a6463fa056899e22959
PATRICKYEH0207 <a020722115847@gmail.com>
2020-03-01 20:25:50 +0800

    Create resnet101_web.m

---
+++ resnet101_web.m
@@ -0,0 +1,226 @@
+clc;clear;
+fprintf('ResNet 10-fold')
+path='.\imgae';
+figuresdir_AUC='.\AUC\';
+figuresdir_ConfusionMatrix='.\ConfusionMatrix\';
+filename = 'option_num.csv';
+Num = readmatrix(filename);
+MiniBatchSize=Num(1,1);Epochs=Num(1,2);
+InitialLearnRate=Num(1,3);ValidationData=Num(1,4);
+ValidationFrequency=Num(1,5);transfer=Num(1,6);
+Augmenter=Num(1,7);times=Num(1,8);Verbose=Num(1,9);
+filename = 'option_str.csv';
+[num,txt,raw] =xlsread(filename);
+Solver=string(txt(1,1));Plots=string(txt(1,2));
+environment=string(txt(1,3));Shuffle=string(txt(1,4));
+finalcsv={'User Name',path;...
+        'Solver',Solver;...
+        'environment',environment;...
+        'Shuffle',Shuffle;...
+        'Verbose',Verbose;...
+        'transfer',transfer;...
+        'Augmenter',Augmenter;...
+        'Epochs',Epochs;'times',times;...
+        'MiniBatchSize',MiniBatchSize;...
+        'ValidationFrequency',ValidationFrequency;...
+        'InitialLearnRate',InitialLearnRate;...
+        'ValidationData',ValidationData;...
+        };
+File = cellstr(path);
+imds = imageDatastore(File, ...
+    'IncludeSubfolders',true, ...
+    'LabelSource','foldernames');
+imds.ReadFcn = @(imgname) imresize(imread(imgname), [224,224]);
+for time=1:times
+        [imdsTrain,imdsValidation] = splitEachLabel(imds,ValidationData,'randomized');
+        if transfer==1
+            net = resnet101;
+            inputSize = net.Layers(1).InputSize;
+            %analyzeNetwork(net)
+            %% Extract the layer graph from the trained network and plot the layer graph.
+            lgraph = layerGraph(net);
+            %% Replace Final Layers
+            lgraph = removeLayers(lgraph, {'fc1000','prob','ClassificationLayer_predictions'});
+            %fc,sofmax,Classification
+            numClasses = numel(categories(imdsTrain.Labels));
+            newLayers = [
+                fullyConnectedLayer(numClasses,'Name','fc','WeightLearnRateFactor',10,'BiasLearnRateFactor',10)
+                softmaxLayer('Name','softmax')
+                classificationLayer('Name','classoutput')];
+            lgraph = addLayers(lgraph,newLayers);
+            lgraph = connectLayers(lgraph,'pool5','fc');
+        else
+               layers = [
+                imageInputLayer([227 227 3])%1
+
+                convolution2dLayer(11,96,'Stride',4)%2
+                reluLayer%3
+                crossChannelNormalizationLayer(5)%4
+                maxPooling2dLayer(3,'Stride',2)%5
+
+                convolution2dLayer(5,256,'Stride',1,'Padding',2)%6
+                reluLayer%7
+                crossChannelNormalizationLayer(5)%8
+                maxPooling2dLayer(3,'Stride',2)%9
+
+                convolution2dLayer(3,384,'Stride',1,'Padding',1)%10
+                reluLayer%11
+                convolution2dLayer(3,384,'Stride',1,'Padding',2)%12
+                reluLayer%13
+                convolution2dLayer(3,256,'Stride',1,'Padding',2)%14
+                reluLayer%15
+                maxPooling2dLayer(3,'Stride',2)%16
+                fullyConnectedLayer(4096)%17
+                reluLayer%18
+                dropoutLayer%19
+                fullyConnectedLayer(4096)%20
+                reluLayer%21
+                dropoutLayer%22
+                fullyConnectedLayer(2)%23
+                softmaxLayer%24
+                classificationLayer%25
+                ];
+            inputSize = layers(1,1).InputSize; 
+        end
+        pixelRange = [-5 5];
+        if Augmenter==1
+            imageAugmenter = imageDataAugmenter( ...
+                'RandXTranslation',pixelRange, ...%X軸平移
+                'RandYTranslation',pixelRange);%Y軸平移
+                %'RandXReflection',true, ...%X軸對稱,也可選RandYReflection
+                         %'RandRotation',[0 360], ...%圖像旋轉角度
+                        %'RandScale',[0.5 1],...%縮小~放大倍率        
+            augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
+                        'DataAugmentation',imageAugmenter);
+            augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
+        else
+            augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain);
+            augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
+        end
+        %-----traning-----%
+        Verbose=logical(mod(Verbose,0));
+        options = trainingOptions(Solver, ...
+                'MiniBatchSize',MiniBatchSize, ...
+                'MaxEpochs',Epochs, ...
+                'InitialLearnRate',InitialLearnRate, ...
+                'Shuffle',Shuffle, ...
+                'ValidationData',augimdsValidation, ...
+                'ValidationFrequency',ValidationFrequency, ...
+                'Verbose',Verbose, ...
+                'ExecutionEnvironment',environment,...
+                'OutputFcn',@(info)savetrainingplot(info),...
+                'Plots',Plots);%
+            diary DiaryFile.txt
+            netTransfer = trainNetwork(augimdsTrain,lgraph,options);
+            %savefig(netTransfer,'training-progress.png')
+            diary off
+            %type myDiaryFile.xlsx
+            [YPred,scores] = classify(netTransfer,augimdsValidation);
+            YValidation = imdsValidation.Labels;
+            finalcsv(16,time+1) = num2cell(mean(YPred == YValidation));
+    %erro image output
+            for i=1:augimdsValidation.NumObservations
+                file_erro = char(imdsValidation.Files(i));
+                [filepath_erro,name,ext] = fileparts(file_erro);
+                test_Name=cellstr(YPred(i));Validation_Name=cellstr(YValidation(i));
+                Compare=strcmp(test_Name,Validation_Name);
+                if Compare==0
+                    image=imread(file_erro);
+                    if transfer==1
+                        imwrite(image,['.\erro\ResNet101_',num2str(time),'_',char(Validation_Name),'_',name,ext]);
+                    else
+                        imwrite(image,['.\erro\nonResNet101_',num2str(time),'_',char(Validation_Name),'_',name,ext]);
+                    end
+                end
+            end
+            %-----confusion matrix-----%
+            C = confusionmat(YValidation,YPred);
+            plotconfusion(YValidation,YPred)
+            if transfer==1
+                filename=['ConfusionMatrix_ResNet101_',num2str(time),'.png'];
+                saveas(gcf,strcat(figuresdir_ConfusionMatrix,filename))
+            else
+                filename=['ConfusionMatrix_nonResNet101_',num2str(time),'.png'];
+                saveas(gcf,strcat(figuresdir_ConfusionMatrix,filename))
+            end
+            TP=C(1,1);FN=C(2,1);FP=C(1,2);TN=C(2,2);
+            TPR=TP/(TP+FN);%sensitivity敏感度
+            FPR=FP/(FP+TN);%1-specificity特異度
+            finalcsv{14,1}='sensitivity';
+            finalcsv{15,1}='1-specificity';
+            finalcsv(14,time+1)=num2cell(TPR);
+            finalcsv(15,time+1)=num2cell(TPR);
+%-----roc cruve-----% 
+            YPred = predict(netTransfer, augimdsValidation);
+            YTest = zeros(size(YPred));
+            scores = zeros(size(imdsValidation.Labels));
+            labels = zeros(size(imdsValidation.Labels));
+            a = dir(char(File));
+            for n=1:length(imdsValidation.Labels)
+                scores(n) = YPred(n,1);
+                    if imdsValidation.Labels(n) == a(length(a)-1).name
+                        YTest(n,1) = 1;
+                        labels(n) = true;
+                    elseif imdsValidation.Labels(n) == a(length(a)).name
+                        YTest(n,2) = 1;
+                        labels(n) = false;
+                    end
+            end
+            [X,Y,T,AUC] = perfcurve(labels==1,scores,'true');
+            figure
+            plot(X,Y)
+            xlabel('False positive rate') 
+            ylabel('True positive rate')
+            if transfer==1
+                filename=['AUC_ResNet101_',num2str(time),'.png'];
+                saveas(gcf,strcat(figuresdir_AUC,filename))
+            end
+            if transfer==0
+                filename=['AUC_nonAlxe_',num2str(time),'.png'];
+                saveas(gcf,strcat(figuresdir_AUC,filename))
+            end
+end
+finalcsv{16,1}='ACC';
+%creat csv
+if transfer==1
+    fid = fopen('.\ACC\ResNet101net.csv','w');
+    for i=1:4
+        fprintf(fid,'%s,%s\n',finalcsv{i,1},finalcsv{i,2});
+    end
+    for i=5:11
+        fprintf(fid,'%s,%i\n',finalcsv{i,1},finalcsv{i,2});
+    end
+	for i=12:13
+        fprintf(fid,'%s,%f\n',finalcsv{i,1},finalcsv{i,2});
+    end
+	for i=14:16
+        fprintf(fid,'%s,',finalcsv{i,1});
+        for j=2:time+1
+            fprintf(fid,'%f,',finalcsv{i,j});
+        end
+        fprintf(fid,'\n');
+	end
+    fclose(fid);
+    close all;
+else 
+    fid = fopen('.\ACC\nonResNet101net.csv','w');
+     for i=1:5
+        fprintf(fid,'%s,%s\n',finalcsv{i,1},finalcsv{i,2});
+    end
+    for i=6:11
+        fprintf(fid,'%s,%i\n',finalcsv{i,1},finalcsv{i,2});
+    end
+	for i=12:13
+        fprintf(fid,'%s,%f\n',finalcsv{i,1},finalcsv{i,2});
+    end
+	for i=14:16
+        fprintf(fid,'%s,',finalcsv{i,1});
+        for j=2:time+1
+            fprintf(fid,'%f,',finalcsv{i,j});
+        end
+        fprintf(fid,'\n');
+    end
+    fclose(fid);
+    close all; 
+end
+%system('start chrome "localhost/transfer/cmd_matlab.php"');
